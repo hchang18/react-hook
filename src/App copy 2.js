d488a8f9b25519adab9b21526bba42e02d16ad32
Haeyoon Chang <haeyoon@pdx.edu>
2021-03-09 08:02:58 -0800

    use notification added

--- src/App copy 2.js
+++ src/App copy 2.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDom from "react-dom";
 
+// notification API will be used 
+
+const useNotification = (title, options) => { 
+  if (!("Notification" in window))
+}
 
 const App = () => {
 
